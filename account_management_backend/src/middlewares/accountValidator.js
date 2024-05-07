46f133d9fa72975770ac8955659b8fdd1f61eb64
teerthrajverma01 <teerthrajverma181001@gmail.com>
2024-05-07 16:15:07 +0530

    modified validation

--- account_management_backend/src/middlewares/accountValidator.js
+++ account_management_backend/src/middlewares/accountValidator.js
@@ -1,13 +1,70 @@
 const { body } = require("express-validator");
 
-const accountValidator = [
+const accountAddValidator = [
+  body("account_no")
+    .trim()
+    .notEmpty()
+    .withMessage("account_no cannot be empty")
+    .isNumeric()
+    .withMessage("account no should be number")
+    .isLength({ min: 12, max: 15 })
+    .withMessage("should be 12-15 digit"),
+  body("email_id")
+    .trim()
+    .notEmpty()
+    .withMessage("email cannot be empty")
+    .isEmail()
+    .withMessage("email must contain @gmail.com"),
+  body("contact_no")
+    .trim()
+    .notEmpty()
+    .withMessage("phoneno cannot be empty")
+    .custom((value) => {
+      const phoneRegex = /^(?:\+?\d{1,3}\s?)?(?:\d{10})$/;
+      if (!phoneRegex.test(value)) {
+        throw new Error("invalid phoneno");
+      }
+      return true;
+    }),
+  body("branch_code")
+    .trim()
+    .notEmpty()
+    .withMessage("branch code cannot be empty")
+    .custom((value) => {
+      if (!/^[A-Z]{2}[0-9]{4}$/.test(value)) {
+        throw new Error("Branch code format is invalid");
+      }
+      return true;
+    }),
+  body("account_type")
+    .trim()
+    .toUpperCase()
+    .custom((value) => {
+      let type_enum = ["SAVING", "CURRENT"];
+      if (!type_enum.includes(value)) {
+        throw new Error("account type not appropiate");
+      }
+      return true;
+    }),
+  body("balance_amount")
+    .optional()
+    .isFloat()
+    .withMessage("balance should be number")
+    .custom((value) => {
+      if (value < 0) {
+        throw new Error("balance cannot be negative");
+      }
+      return true;
+    }),
+];
+const accountUpdateValidator = [
   body("account_no")
     .trim()
     .notEmpty()
     .isNumeric()
     .withMessage("account no should be number")
     .isLength({ min: 12, max: 15 })
-    .withMessage("should be 12 digit"),
+    .withMessage("should be 12-15 digit"),
   body("email_id")
     .optional()
     .trim()
@@ -38,5 +95,15 @@ const accountValidator = [
       }
       return true;
     }),
+  body("balance_amount")
+    .optional()
+    .isFloat()
+    .withMessage("balance should be number")
+    .custom((value) => {
+      if (value < 0) {
+        throw new Error("balance cannot be negative");
+      }
+      return true;
+    }),
 ];
-module.exports = accountValidator;
+module.exports = { accountAddValidator, accountUpdateValidator };
--- account_management_backend/src/routes/account.routes.js
+++ account_management_backend/src/routes/account.routes.js
@@ -9,12 +9,15 @@ const {
   deleteExistingAccount,
 } = require("../controllers/account.controller");
 
-const accountValidator = require("../middlewares/accountValidator");
+const {
+  accountAddValidator,
+  accountUpdateValidator,
+} = require("../middlewares/accountValidator");
 // ######account#########
 router.route("/account").get(getAllAccount);
 router.route("/account/:account_no").get(getAccountByAccountNo);
-router.route("/account").post(accountValidator, addNewAccount);
-router.route("/account").put(accountValidator, updateExistingAccount);
+router.route("/account").post(accountAddValidator, addNewAccount);
+router.route("/account").put(accountUpdateValidator, updateExistingAccount);
 router.route("/account/:account_no").delete(deleteExistingAccount);
 
 module.exports = router;
