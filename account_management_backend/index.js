9e863a3980909d8ebb06749b3baa6c922ffa51c5
teerthrajverma01 <teerthrajverma181001@gmail.com>
2024-05-07 11:13:12 +0530

    initialized backend app and basic configuration

---
+++ account_management_backend/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+// ************************************************************
+const app = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.urlencoded({ extended: false, limit: "16kb" }));
+app.use(cookieParser());
+app.use(express.json({ limit: "16kb" }));
+app.use(express.static("public"));
+
+// *************************************************************
+
+//Api Routes
+// admin router
+const adminRouter = require("./src/routes/admin.routes");
+app.use("/api/admin/", adminRouter);
+
+// **************************************************************
+
+// error middleware
+const errorHandler = require("./src/middlewares/error.middleware");
+app.use(errorHandler);
+
+// **************************************************************
+
+// post running at 3000
+const backendPort = process.env.PORT || 3000;
+app.listen(backendPort, () => {
+  console.log(`Server is running at http://localhost:${backendPort}`);
+});
+
+module.exports = app; //for testing
+
+// ***********************************
