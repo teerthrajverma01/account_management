9e0daf8e417fb7313e43af284e2e65fbf56d527a
teerthrajverma01 <teerthrajverma181001@gmail.com>
2024-05-07 11:43:44 +0530

    added routes

--- account_management_backend/src/routes/account.routes.js
+++ account_management_backend/src/routes/account.routes.js
@@ -2,6 +2,10 @@ const { Router } = require("express");
 const router = Router();
 
 // ######account#########
-router.route("").get();
+router.route("/account").get();
+router.route("/account/:account_no").get();
+router.route("/account").post();
+router.route("/account").put();
+router.route("/account").delete();
 
 module.exports = router;
